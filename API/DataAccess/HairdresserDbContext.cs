0e4627d7a2c07080d2c1b0c38e817524e0399622
tomas.ondrejka2000@gmail.com <304150@via.dk>
2022-01-03 16:30:36 +0100

    Creating WorkingDay.cs and Appointment.cs as database tables

--- API/DataAccess/HairdresserDbContext.cs
+++ API/DataAccess/HairdresserDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using API.Controllers;
 using API.Models;
+using Client.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.DataAccess
@@ -9,6 +10,8 @@ namespace API.DataAccess
         public DbSet<User> Users { get; set; }
         public DbSet<Company> Companies { get; set; }
         public DbSet<Product> Products { get; set; }
+        public DbSet<WorkingDay> WorkingDays { get; set; }
+        public DbSet<Appointment> Appointments { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
