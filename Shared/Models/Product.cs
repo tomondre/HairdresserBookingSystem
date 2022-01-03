a9cb01755ed17994f723ee464d31038466d077c1
tomas.ondrejka2000@gmail.com <304150@via.dk>
2022-01-03 15:29:18 +0100

    Adding price information to the product

--- Shared/Models/Product.cs
+++ Shared/Models/Product.cs
@@ -8,7 +8,11 @@ namespace API.Models
         public int Id { get; set; }
         [Required, MaxLength(100)]
         public string Name { get; set; }
+        [Required, MaxLength(300)]
         public string Description { get; set; }
+        [Required]
+        public double Price { get; set; }
+        
         [Required, Range(10, 180)]
         public int ProcedureLengthInMinutes { get; set; }
         public int CompanyId { get; set; }
