46ae4a01e84f9e818fa02c83d4d34f320d2a4bb2
tomas.ondrejka2000@gmail.com <304150@via.dk>
2022-01-03 12:47:04 +0100

    Creating WorkSchedule object that represents when the company has opening hours

---
+++ Client/Models/WorkSchedule.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Client.Models
+{
+    public class WorkSchedule : Appointment
+    {
+        public override DateTime End { get; set; }
+        public override string Text { get; set; }
+
+    }
+}
