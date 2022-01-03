e5103ff637b3de5039c2bcd3ee1ac03e76dc65dd
tomas.ondrejka2000@gmail.com <304150@via.dk>
2022-01-03 12:47:57 +0100

    Creating IProduct service that will ahndle requests related to Products

---
+++ Client/Data/Products/IProductService.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Client.Models;
+
+namespace Client.Data
+{
+    public interface IProductService
+    {
+        Task<ProductList> GetAllCompanyProducts(int id);
+    }
+}
---
+++ Client/Data/Products/ProductService.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using Client.Models;
+
+namespace Client.Data
+{
+    public class ProductService : IProductService
+    
+    {
+        public Task<ProductList> GetAllCompanyProducts(int id)
+        {
+            return null;
+        }
+    }
+}
