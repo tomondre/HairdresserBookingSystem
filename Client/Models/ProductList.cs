3004cb39efbfc7d6e198462dde9e269c1f7d0ea0
tomas.ondrejka2000@gmail.com <304150@via.dk>
2022-01-03 12:46:33 +0100

    Creating List wrappers

---
+++ Client/Models/ProductList.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using API.Models;
+
+namespace Client.Models
+{
+    public class ProductList
+    {
+        public IList<Product> Products { get; set; }
+
+        public ProductList()
+        {
+            Products = new List<Product>();
+        }
+
+    }
+}
---
+++ Client/Models/WorkingDayList.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace Client.Models
+{
+    public class WorkingDayList
+    {
+        public IList<WorkingDay> Days { get; set; }
+
+        public IList<Appointment> Appointments
+        {
+            get
+            {
+                var result = new List<Appointment>();
+                foreach (var day in Days)
+                {
+                    result.AddRange(day.Appointments);
+                }
+
+                return result;
+            }
+        }
+
+        public WorkingDayList()
+        {
+            Days = new List<WorkingDay>();
+        }
+    }
+}
