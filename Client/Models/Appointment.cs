a2bfb42497f12f6c54ebb066ece9435130e3eac9
tomas.ondrejka2000@gmail.com <304150@via.dk>
2022-01-03 10:21:51 +0100

    Refactor

---
+++ Client/Models/Appointment.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using API.Models;
+
+namespace Client.Models
+{
+    public class Appointment
+    {
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+        public string Text { get; set; }
+        public TimeSlot TimeSlot { get; set; }
+        public Product Product { get; set; }
+    }
+}
