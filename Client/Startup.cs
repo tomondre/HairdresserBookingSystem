03e61d31a7bab2cf93b8d9084f94727c32327fa2
tomas.ondrejka2000@gmail.com <304150@via.dk>
2022-01-03 12:48:46 +0100

    Registering newly created services

--- Client/Startup.cs
+++ Client/Startup.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Client.Data;
+using Client.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
@@ -9,7 +11,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Client.Data;
+using Radzen;
 
 namespace Client
 {
@@ -28,7 +30,11 @@ namespace Client
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
-            services.AddSingleton<WeatherForecastService>();
+
+            services.AddScoped<NotificationService>();
+            services.AddScoped<DialogService>();
+
+            services.AddScoped<IAppointmentService, AppointmentService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
