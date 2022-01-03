e64f06edfb03914cab51fc8781690419c1a13af5
tomas.ondrejka2000@gmail.com <304150@via.dk>
2022-01-03 21:03:41 +0100

    Refactoring controller method url

--- API/Controllers/WorkingDaysController.cs
+++ API/Controllers/WorkingDaysController.cs
@@ -33,7 +33,7 @@ namespace API.Controllers
             }
         }
 
-        [HttpGet("/companies/{id:int}/workingsdays")]
+        [HttpGet("/companies/{id:int}/workingdays")]
         public async Task<ActionResult<WorkingDayList>> GetAllCompanyWorkingDaysAsync([FromRoute] int id)
         {
             try
