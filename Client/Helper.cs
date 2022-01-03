e943d18a06e37205027a0d50bbfe450a671039c1
tomas.ondrejka2000@gmail.com <304150@via.dk>
2022-01-03 15:28:05 +0100

    Creating Helper class

---
+++ Client/Helper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.Json;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace Client
+{
+    public class Global
+    {
+        public static string url  = "https://localhost:5003";
+
+        public static void CheckException(HttpResponseMessage response)
+        {
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception(response.Content.ToString());
+            }
+        }
+
+        public static T Deserialize<T>(string s)
+        {
+            var deserialize = JsonSerializer.Deserialize<T>(s, new JsonSerializerOptions()
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            });
+            return deserialize;
+        }
+    }
+}
