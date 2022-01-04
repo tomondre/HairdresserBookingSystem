f799e56a6b21d7b9b1d16626cf64a113123022f5
tomas.ondrejka2000@gmail.com <304150@via.dk>
2022-01-04 15:37:39 +0100

    Splitting model classes

---
+++ API/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        
+        public int ProcedureLengthInMinutes { get; set; }
+        public int CompanyId { get; set; }
+    }
+}
---
+++ API/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public string UserType { get; set; }
+    }
+}
