e9e2e59b61130d65c004ae73c70ea866ac629b0d
tomas.ondrejka2000@gmail.com <304150@via.dk>
2022-01-04 21:24:23 +0100

    Registering classes to db context

--- API/DataAccess/HairdresserDbContext.cs
+++ API/DataAccess/HairdresserDbContext.cs
@@ -2,12 +2,15 @@
 using API.Models;
 using Client.Models;
 using Microsoft.EntityFrameworkCore;
+using Shared.Models;
 
 namespace API.DataAccess
 {
     public class HairdresserDbContext : DbContext
     {
         public DbSet<User> Users { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<CompanyOwner> CompanyOwners { get; set; }
         public DbSet<Company> Companies { get; set; }
         public DbSet<Product> Products { get; set; }
         public DbSet<WorkingDay> WorkingDays { get; set; }
