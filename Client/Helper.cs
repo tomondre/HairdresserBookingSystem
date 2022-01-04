08d67552a0baa2b902067f2af17caf16382d5dbe
tomas.ondrejka2000@gmail.com <304150@via.dk>
2022-01-04 15:38:30 +0100

    Helper now contains method for hashing password

--- Client/Helper.cs
+++ Client/Helper.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Net.Http;
+using System.Security.Cryptography;
+using System.Text;
 using System.Text.Json;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
 
@@ -34,5 +36,19 @@ namespace Client
             });
             return serialize;
         }
+        
+        public static string Hash(string rawData) {  
+            using (SHA256 sha256Hash = SHA256.Create())  
+            {  
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(rawData));  
+  
+                StringBuilder builder = new StringBuilder();  
+                for (int i = 0; i < bytes.Length; i++)  
+                {  
+                    builder.Append(bytes[i].ToString("x2"));  
+                }  
+                return builder.ToString();  
+            }  
+        }  
     }
 }
