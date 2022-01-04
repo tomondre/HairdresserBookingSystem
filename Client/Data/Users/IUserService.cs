9663a9340d75c21b363c2f4138d90dca6014f4aa
tomas.ondrejka2000@gmail.com <304150@via.dk>
2022-01-04 15:37:15 +0100

    Creating User service

---
+++ Client/Data/Users/IUserService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using API.Models;
+
+namespace Client.Data.Users
+{
+    public interface IUserService
+    {
+        Task<User> TryLoginUserAsync(User user);
+    }
+}
---
+++ Client/Data/Users/UserService.cs
@@ -0,0 +1,28 @@
+﻿using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using API.Models;
+
+namespace Client.Data.Users
+{
+    public class UserService : IUserService
+    {
+        private HttpClient client;
+
+        public UserService(HttpClient client)
+        {
+            this.client = client;
+        }
+        
+        public async Task<User> TryLoginUserAsync(User user)
+        {
+            var serialize = Helper.Serialize(user);
+            var stringContent = new StringContent(serialize, Encoding.UTF8, "application/json");
+            var postAsync = await client.PostAsync($"{Helper.url}/login", stringContent);
+            Helper.CheckException(postAsync);
+            var readAsStringAsync = await postAsync.Content.ReadAsStringAsync();
+            var deserialize = Helper.Deserialize<User>(readAsStringAsync);
+            return deserialize;
+        }
+    }
+}
