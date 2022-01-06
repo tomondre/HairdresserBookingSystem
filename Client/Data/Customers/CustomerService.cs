7c0885fd0a68fede1305feeac8a9e57687f23a88
tomas.ondrejka2000@gmail.com <304150@via.dk>
2022-01-06 22:01:37 +0100

    Implementing CreateProductAsync method

---
+++ Client/Data/Customers/CustomerService.cs
@@ -0,0 +1,29 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Shared.Models;
+
+namespace Client.Data.Customers
+{
+    public class CustomerService : ICustomerService
+    {
+        private HttpClient client;
+        
+        public CustomerService(HttpClient client)
+        {
+            this.client = client;
+        }
+
+        public async Task<Customer> CreateCustomerAsync(Customer customer)
+        {
+            var serialize = Helper.Serialize(customer);
+            var stringContent = new StringContent(serialize, Encoding.UTF8, "application/json");
+            var postAsync = await client.PostAsync($"{Helper.url}/customers", stringContent);
+            Helper.CheckException(postAsync);
+            var readAsStringAsync = await postAsync.Content.ReadAsStringAsync();
+            var deserialize = Helper.Deserialize<Customer>(readAsStringAsync);
+            return deserialize;
+        }
+    }
+}
---
+++ Client/Data/Customers/ICustomerService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using Shared.Models;
+
+namespace Client.Data.Customers
+{
+    public interface ICustomerService
+    {
+        Task<Customer> CreateCustomerAsync(Customer customer);
+    }
+}
