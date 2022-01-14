de5122860de1a026aa7a9ce3972c37d83db7b27f
tomas.ondrejka2000@gmail.com <304150@via.dk>
2022-01-14 14:10:38 +0100

    Removing create company dao

--- API/Persistence/Companies/CompanyDao.cs
+++ API/Persistence/Companies/CompanyDao.cs
@@ -12,21 +12,6 @@ namespace API.Persistence
 {
     public class CompanyDao : PaginationHelper, ICompanyDao
     {
-        public async Task<Company> CreateCompanyAsync(Company company)
-        {
-            await using HairdresserDbContext context = new HairdresserDbContext();
-            var firstOrDefaultAsync = await context.Users.OfType<CompanyOwner>().FirstOrDefaultAsync((user) => user.Id == company.User.Id);
-            if (firstOrDefaultAsync == null)
-            {
-                throw new Exception("User doesnt exist");
-            }
-
-            company.User = firstOrDefaultAsync;
-            var entityEntry = context.Companies.Add(company);
-            await context.SaveChangesAsync();
-            return entityEntry.Entity;
-        }
-
         public async Task<Company> GetCompanyByIdAsync(int id)
         {
             await using HairdresserDbContext context = new HairdresserDbContext();
