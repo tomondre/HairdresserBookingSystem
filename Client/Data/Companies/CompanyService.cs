b46711819afff80e5319e40ab970ab157a5378a9
tomas.ondrejka2000@gmail.com <304150@via.dk>
2022-01-14 12:14:53 +0100

    Creating Company Service

---
+++ Client/Data/Companies/CompanyService.cs
@@ -0,0 +1,26 @@
+﻿
+using System.Net.Http;
+using System.Threading.Tasks;
+using Shared.Models;
+
+namespace Client.Data.Companies
+{
+    public class CompanyService : ICompanyService
+    {
+        private HttpClient client;
+
+        public CompanyService(HttpClient client)
+        {
+            this.client = client;
+        }
+        
+        public async Task<Company> GetCompanyByIdAsync(int id)
+        {
+            var httpResponseMessage = await client.GetAsync($"{Helper.url}/companies");
+            Helper.CheckException(httpResponseMessage);
+            var readAsStringAsync = await httpResponseMessage.Content.ReadAsStringAsync();
+            var deserialize = Helper.Deserialize<Company>(readAsStringAsync);
+            return deserialize;
+        }
+    }
+}
---
+++ Client/Data/Companies/ICompanyService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using Shared.Models;
+
+namespace Client.Data.Companies
+{
+    public interface ICompanyService
+    {
+        Task<Company> GetCompanyByIdAsync(int id);
+    }
+}
