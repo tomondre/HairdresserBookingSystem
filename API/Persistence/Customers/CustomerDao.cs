b322ec0dcd18af1aa364705b73d15f36a5f318a4
tomas.ondrejka2000@gmail.com <304150@via.dk>
2022-01-14 12:48:01 +0100

    Adding check if the customer with the given email already exist in the system

--- API/Persistence/Customers/CustomerDao.cs
+++ API/Persistence/Customers/CustomerDao.cs
@@ -1,6 +1,8 @@
-﻿using System.Security.Cryptography;
+﻿using System;
+using System.Security.Cryptography;
 using System.Threading.Tasks;
 using API.DataAccess;
+using Microsoft.EntityFrameworkCore;
 using Shared.Models;
 
 namespace API.Persistence.Customers
@@ -10,6 +12,11 @@ namespace API.Persistence.Customers
         public async Task<Customer> CreateCustomerAsync(Customer customer)
         {
             using HairdresserDbContext context = new HairdresserDbContext();
+            var firstOrDefaultAsync = await context.Customers.FirstOrDefaultAsync(c => c.Email == customer.Email);
+            if (firstOrDefaultAsync != null)
+            {
+                throw new Exception("Customer with the given email already exist!");
+            }
             var entityEntry = await context.Customers.AddAsync(customer);
             await context.SaveChangesAsync();
             return entityEntry.Entity;
