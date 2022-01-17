5c738067712ff5a2258702222b972290f134ce62
tomas.ondrejka2000@gmail.com <304150@via.dk>
2022-01-17 11:15:40 +0100

    Creating CompanyList model

---
+++ Shared/Models/CompanyList.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Shared.Models
+{
+    public class CompanyList
+    {
+        public IList<Company> Companies { get; set; }
+
+        public CompanyList()
+        {
+            Companies = new List<Company>();
+        }
+    }
+}
