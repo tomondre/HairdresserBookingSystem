f78934bc8207e74cce566192a04c378884a243d3
tomas.ondrejka2000@gmail.com <304150@via.dk>
2022-02-12 18:19:36 +0100

    Creating AppointmentList.cs class

---
+++ Client/Models/AppointmentList.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Client.Models
+{
+    public class AppointmentList
+    {
+        public IList<Appointment> Appointments { get; set; }
+
+        public AppointmentList()
+        {
+            Appointments = new List<Appointment>();
+        }
+    }
+}
