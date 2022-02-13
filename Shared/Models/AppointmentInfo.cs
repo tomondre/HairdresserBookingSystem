9af84a3b9f233dae8ed67301a4dac7f8d39aba58
tomas.ondrejka2000@gmail.com <304150@via.dk>
2022-02-13 13:22:43 +0100

    Creating AppointmentInfo model classes

---
+++ Shared/Models/AppointmentInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Shared.Models
+{
+    public class AppointmentInfo
+    {
+        public DateTime AvailableTime { get; set; }
+        public int MaxProductLength { get; set; }
+
+        public AppointmentInfo()
+        {
+        }
+    }
+}
---
+++ Shared/Models/AppointmentInfoList.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Shared.Models
+{
+    public class AppointmentInfoList
+    {
+        public IList<AppointmentInfo> AppointmentInfos { get; set; }
+
+        public AppointmentInfoList()
+        {
+            AppointmentInfos = new List<AppointmentInfo>();
+        }
+    }
+}
