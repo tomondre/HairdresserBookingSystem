528162a91fe66e21a39ee440390579f928904079
tomas.ondrejka2000@gmail.com <304150@via.dk>
2022-02-13 13:31:54 +0100

    Adding method GetCompanyAppointmentInfosAsync

--- API/Model/Appointments/AppointmentModel.cs
+++ API/Model/Appointments/AppointmentModel.cs
@@ -1,16 +1,23 @@
-﻿using System.Threading.Tasks;
+﻿using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using API.Model.WorkingDays;
 using API.Persistence.Appointments;
 using Client.Models;
+using Shared.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
 
 namespace API.Model.Appointments
 {
     public class AppointmentModel : IAppointmentModel
     {
         private IAppointmentDao dao;
+        private IWorkingDayModel workingDayModel;
 
-        public AppointmentModel(IAppointmentDao dao)
+        public AppointmentModel(IAppointmentDao dao, IWorkingDayModel workingDayModel)
         {
             this.dao = dao;
+            this.workingDayModel = workingDayModel;
         }
 
         public Task<Appointment> CreateAppointmentAsync(Appointment appointment)
@@ -26,6 +33,29 @@ namespace API.Model.Appointments
         public Task<Appointment> DeleteAppointmentAsync(int id)
         {
             return dao.DeleteAppointmentAsync(id);
+        }
+
+        public async Task<AppointmentInfoList> GetCompanyAppointmentInfosAsync(int id)
+        {
+            var allCompanyWorkingDaysAsync = await workingDayModel.GetAllCompanyWorkingDaysAsync(id);
+
+            var appointmentInfoList = new AppointmentInfoList();
+            foreach (var day in allCompanyWorkingDaysAsync.Days)
+            {
+                var appointment = day.Appointments.Last();
+                var lastTime = appointment.Start.AddMinutes(appointment.Product.ProcedureLengthInMinutes);
+                
+                // Do not forget about the case when break occur
+                
+                // We need to create some kind of offset because the breaks wont be perfectly times - each product has different length in minutes
+                
+                // Calculate difference between lastTime and working day end time
+
+                // Check if the result is smaller than zero, if so do not create appointment info
+                
+            }
+
+            throw new System.NotImplementedException();
         }
     }
 }
--- API/Model/Appointments/IAppointmentModel.cs
+++ API/Model/Appointments/IAppointmentModel.cs
@@ -1,5 +1,6 @@
 ﻿using System.Threading.Tasks;
 using Client.Models;
+using Shared.Models;
 
 namespace API.Model.Appointments
 {
@@ -8,5 +9,6 @@ namespace API.Model.Appointments
         Task<Appointment> CreateAppointmentAsync(Appointment appointment2);
         Task<AppointmentList> GetAllCompanyAppointmentsAsync(int id);
         Task<Appointment> DeleteAppointmentAsync(int id);
+        Task<AppointmentInfoList> GetCompanyAppointmentInfosAsync(int id);
     }
 }
